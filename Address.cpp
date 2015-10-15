cd5cae3d94809c9b645687b926125ab162accd14
Vitor <trabalho@vmpaisante.com>
2015-10-15 02:45:04 -0300

    formated Address

--- Address.cpp
+++ Address.cpp
@@ -13,24 +13,22 @@
 
 using namespace llvm;
 
-//Contructors and destructors
-Address::Address(RangedPointer* a, RangedPointer* b, Offset o)
-{
+// Contructors and destructors
+Address::Address(RangedPointer *a, RangedPointer *b, Offset o) {
   addressee = a;
   base = b;
   offset = o;
   widened = false;
   addressee->addresses.insert(this);
   base->bases.insert(this);
-  
-  if(isa<const Argument>(*(base->getPointer())))
-  	argument = true;
+
+  if (isa<const Argument>(*(base->getPointer())))
+    argument = true;
   else
-  	argument = false;
+    argument = false;
 }
 
-Address::Address(Address* copy)
-{
+Address::Address(Address *copy) {
   base = copy->base;
   addressee = copy->addressee;
   offset = copy->offset;
@@ -39,54 +37,33 @@ Address::Address(Address* copy)
   argument = copy->argument;
   narrowingOps = copy->narrowingOps;
   wideningOps = copy->wideningOps;
-  
+
   addressee->addresses.insert(this);
   base->bases.insert(this);
-  
 }
 
-Address::~Address()
-{
+Address::~Address() {
   addressee->addresses.erase(this);
   base->bases.erase(this);
 }
 
-//Functions that provide the object's information
-RangedPointer* Address::getBase()
-{
-  return base;
-}
+// Functions that provide the object's information
+RangedPointer *Address::getBase() { return base; }
 
-RangedPointer* Address::getAddressee()
-{
-  return addressee;
-}
+RangedPointer *Address::getAddressee() { return addressee; }
 
-Offset Address::getOffset()
-{
-  return offset;
-}
+Offset Address::getOffset() { return offset; }
 
-bool Address::wasWidened()
-{
-  return widened;
-}
+bool Address::wasWidened() { return widened; }
 
-//Functions that set the object's information
-void Address::setBase(RangedPointer* b)
-{
-  base = b;
-}
+// Functions that set the object's information
+void Address::setBase(RangedPointer *b) { base = b; }
 
-void Address::setOffset(Offset o)
-{
-  offset = o;
-}
+void Address::setOffset(Offset o) { offset = o; }
 
-//Function tha prints the object's informtation
-void Address::print()
-{
-  if(base->getPointer()->getValueName() == NULL)
+// Function tha prints the object's informtation
+void Address::print() {
+  if (base->getPointer()->getValueName() == NULL)
     errs() << *(base->getPointer());
   else
     errs() << base->getPointer()->getName();
@@ -95,109 +72,102 @@ void Address::print()
   offset.print();
 }
 
-//Function that expands an address, this is the most important feature
+// Function that expands an address, this is the most important feature
 //  of this class.
-void Address::Expand (std::deque<Address*> &adq, std::set<Address*> &fn)
-{
-  //Was the base already expanded, if no expansion can occur.
+void Address::Expand(std::deque<Address *> &adq, std::set<Address *> &fn) {
+  // Was the base already expanded, if no expansion can occur.
   //  If it was expanded, a new widening op must be created
-  if(expanded.find(base) == expanded.end())
-  {
-    std::set<Address*> aux = base->addresses;
-    for(auto i = aux.begin(), ie = aux.end(); i != ie; i++)
-    {
-      //For each address from base, a new address must be formed
+  if (expanded.find(base) == expanded.end()) {
+    std::set<Address *> aux = base->addresses;
+    for (auto i = aux.begin(), ie = aux.end(); i != ie; i++) {
+      // For each address from base, a new address must be formed
       //  combining each with this address
-      Address* ad = (*i); 
+      Address *ad = (*i);
       Offset r = ad->offset + offset;
-      Address* newAddress = new Address(addressee, ad->base, r);
-      
-      //If there is an argument flag in any of the addresses,
+      Address *newAddress = new Address(addressee, ad->base, r);
+
+      // If there is an argument flag in any of the addresses,
       // then the new address will have the argument flag
-      if(argument) newAddress->argument = true;
-      else if(ad->argument) newAddress->argument = true;
-      
-      //The expanded set of the new address will have all the pointers
+      if (argument)
+        newAddress->argument = true;
+      else if (ad->argument)
+        newAddress->argument = true;
+
+      // The expanded set of the new address will have all the pointers
       // in the expanded of this address plus it's base
       newAddress->expanded = expanded;
       newAddress->expanded[base] = offset;
-      
-      //If there is a widened flag in any of the addresses,
+
+      // If there is a widened flag in any of the addresses,
       // then the new address will have the widened flag
-      if(widened) newAddress->widened = true;
-      else if(ad->widened) newAddress->widened = true;
-      
-      //The new address will have the same widening operators
+      if (widened)
+        newAddress->widened = true;
+      else if (ad->widened)
+        newAddress->widened = true;
+
+      // The new address will have the same widening operators
       //  and contextualized narrowing operators.
       //  Possible bug: it only inserts the first narrowing operator
-      //  found on the path from the same base pointer, 
-      //  I suspect it is the most restrictive, since 
+      //  found on the path from the same base pointer,
+      //  I suspect it is the most restrictive, since
       //  I haven't found an example that says otherwise.
       newAddress->narrowingOps = narrowingOps;
-      for(auto i : ad->narrowingOps)
-        if(newAddress->narrowingOps.find(i.first) == newAddress->narrowingOps.end())
+      for (auto i : ad->narrowingOps)
+        if (newAddress->narrowingOps.find(i.first) ==
+            newAddress->narrowingOps.end())
           newAddress->narrowingOps[i.first] = i.second.contextualize(offset);
-      
+
       newAddress->wideningOps = wideningOps;
-      for(auto i : ad->wideningOps)
-        if(newAddress->wideningOps.find(i.first) == newAddress->wideningOps.end())
-            newAddress->wideningOps[i.first] = i.second;
-          
+      for (auto i : ad->wideningOps)
+        if (newAddress->wideningOps.find(i.first) ==
+            newAddress->wideningOps.end())
+          newAddress->wideningOps[i.first] = i.second;
+
       adq.push_front(newAddress);
     }
-  }
-  else
-  {
-    //New widening operator
+  } else {
+    // New widening operator
     WideningOp r;
     r.before = expanded.at(base);
     r.after = offset;
     wideningOps[base->getPointer()] = r;
-    for(auto i : adq)
-    {
+    for (auto i : adq) {
       i->widened = true;
-      //Adds the address's narrowing operators to all others
-      for(auto j : narrowingOps)
-        if(i->narrowingOps.find(j.first) == i->narrowingOps.end())
+      // Adds the address's narrowing operators to all others
+      for (auto j : narrowingOps)
+        if (i->narrowingOps.find(j.first) == i->narrowingOps.end())
           i->narrowingOps[j.first] = j.second;
-      //Adds the address's narrowing operators to all others
-      for(auto j : wideningOps)
+      // Adds the address's narrowing operators to all others
+      for (auto j : wideningOps)
         i->wideningOps[j.first] = j.second;
-      //Adds the address's expanded adresses to all others
-      for(auto j : expanded)
-        i->expanded.insert(j);
-      
-    }
-    for(auto i : fn)
-    {
-      i->widened = true;
-      //Adds the address's narrowing operators to all others
-      for(auto j : narrowingOps)
-        if(i->narrowingOps.find(j.first) == i->narrowingOps.end())
-          i->narrowingOps[j.first] = j.second;
-      //Adds the address's narrowing operators to all others
-      for(auto j : wideningOps)
-        i->wideningOps[j.first] = j.second;
-      //Adds the address's expanded adresses to all others
-      for(auto j : expanded)
+      // Adds the address's expanded adresses to all others
+      for (auto j : expanded)
         i->expanded.insert(j);
     }
-    
+    for (auto i : fn) {
+      i->widened = true;
+      // Adds the address's narrowing operators to all others
+      for (auto j : narrowingOps)
+        if (i->narrowingOps.find(j.first) == i->narrowingOps.end())
+          i->narrowingOps[j.first] = j.second;
+      // Adds the address's narrowing operators to all others
+      for (auto j : wideningOps)
+        i->wideningOps[j.first] = j.second;
+      // Adds the address's expanded adresses to all others
+      for (auto j : expanded)
+        i->expanded.insert(j);
+    }
   }
   delete this;
 }
 
-//Functions that give the object narrowing and widening operators
-bool Address::associateNarrowingOp(const Value* v, NarrowingOp& no)
-{
+// Functions that give the object narrowing and widening operators
+bool Address::associateNarrowingOp(const Value *v, NarrowingOp &no) {
   narrowingOps[v] = no;
   return true;
 }
 
-bool Address::associateWideningOp(const Value* v, WideningOp& wo)
-{
+bool Address::associateWideningOp(const Value *v, WideningOp &wo) {
   wideningOps[v] = wo;
   return true;
 }
-
-
--- Address.h
+++ Address.h
@@ -11,16 +11,16 @@
 #ifndef __ADDRESS_H__
 #define __ADDRESS_H__
 
-//Project's includes
+// Project's includes
 #include "RangedPointer.h"
 #include "Offset.h"
 #include "Narrowing.h"
-//llvm's includes
+// llvm's includes
 #include "llvm/IR/Value.h"
 #include "llvm/IR/Argument.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/raw_ostream.h"
-//libc includes
+// libc includes
 #include <set>
 #include <map>
 #include <deque>
@@ -29,47 +29,44 @@ namespace llvm {
 
 /// Representation of a possible pointer address. It is composed,
 /// essencially, of a base pointer and an offset
-class Address
-{
-  public:
-  //Contructors and destructors
-  Address(RangedPointer* a, RangedPointer* b, Offset o);
-  Address(Address*);
+class Address {
+public:
+  // Contructors and destructors
+  Address(RangedPointer *a, RangedPointer *b, Offset o);
+  Address(Address *);
   ~Address();
-  //Functions that provide the object's information
-  RangedPointer* getBase();
-  RangedPointer* getAddressee();
+  // Functions that provide the object's information
+  RangedPointer *getBase();
+  RangedPointer *getAddressee();
   Offset getOffset();
   bool wasWidened();
-  //Functions that set the object's information
-  void setBase(RangedPointer*);
+  // Functions that set the object's information
+  void setBase(RangedPointer *);
   void setOffset(Offset);
-  //Function tha prints the object's informtation
+  // Function tha prints the object's informtation
   void print();
-  //Function that expands an address, this is the most important feature
+  // Function that expands an address, this is the most important feature
   //  of this class.
-  void Expand (std::deque<Address*> &ad, std::set<Address*> &fn);
-  //Functions that give the object narrowing and widening operators
-  bool associateNarrowingOp(const Value*, NarrowingOp&);
-  bool associateWideningOp(const Value*, WideningOp&);
-  
-  private:
-  //Basic contents of an address
-  RangedPointer* base;
-  RangedPointer* addressee;
-  Offset offset;
-  //Structures that hold the narrowing and widening operators
-  std::map<const Value*, NarrowingOp> narrowingOps;
- std::map<const Value*, WideningOp> wideningOps; 
-  //Holds wether this address has been widened
-  bool widened;
-  //Holds wether the base is an argument or there is an argument on the path
-  bool argument;
-  //Auxilliary map for the expand function
-  std::map<RangedPointer*, Offset> expanded;
-  
-};
+  void Expand(std::deque<Address *> &ad, std::set<Address *> &fn);
+  // Functions that give the object narrowing and widening operators
+  bool associateNarrowingOp(const Value *, NarrowingOp &);
+  bool associateWideningOp(const Value *, WideningOp &);
 
+private:
+  // Basic contents of an address
+  RangedPointer *base;
+  RangedPointer *addressee;
+  Offset offset;
+  // Structures that hold the narrowing and widening operators
+  std::map<const Value *, NarrowingOp> narrowingOps;
+  std::map<const Value *, WideningOp> wideningOps;
+  // Holds wether this address has been widened
+  bool widened;
+  // Holds wether the base is an argument or there is an argument on the path
+  bool argument;
+  // Auxilliary map for the expand function
+  std::map<RangedPointer *, Offset> expanded;
+};
 }
 
 #endif
