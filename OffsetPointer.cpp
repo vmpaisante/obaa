6579c11eac7c9aa91a97dc8d5b64a438fa700907
Vitor <vmpaisante@gmail.com>
2015-12-21 19:13:00 -0200

    Bit of work on OffsetPointer.cpp

--- OffsetPointer.cpp
+++ OffsetPointer.cpp
@@ -12,6 +12,8 @@
 // local includes
 #include "OffsetPointer.h"
 #include "Address.h"
+#include "OffsetBasedAliasAnalysis.h"
+#include "Offset.h"
 // llvm includes
 #include "llvm/IR/Value.h"
 #include "llvm/IR/Type.h"
@@ -108,25 +110,96 @@ void OffsetPointer::print() const {
 
 /// \brief Function that finds the pointer's possible addresses,
 void OffsetPointer::processInitialAddresses(OffsetBasedAliasAnalysis* Analysis){
-  if(isa<const GlobalVariable>(*pointer)) { }
-  else if(const Argument* p = dyn_cast<Argument>(pointer)) { }
-  else if(isa<const AllocaInst>(*pointer)) { }
-  else if(const CallInst* p = dyn_cast<CallInst>(pointer)) { }
-  else if(const BitCastInst* p = dyn_cast<BitCastInst>(pointer)) { }
-  else if(isa<const LoadInst>(*pointer)) { }
-  else if(const PHINode* p = dyn_cast<PHINode>(pointer)) { }
-  else if(const GetElementPtrInst* p = dyn_cast<GetElementPtrInst>(pointer)) { }
-  else if(const GEPOperator* p = dyn_cast<GEPOperator>(pointer)) { }
-  else if(isa<const ConstantPointerNull>(*pointer)) { }
-  else if(const ConstantExpr* p = dyn_cast<ConstantExpr>(pointer)) { }
-  else if(isa<const Function>(*pointer)) { }
+  // Global variables in LLVM are pointers by definition with their own alloc
+  if(isa<const GlobalVariable>(*pointer)) { pointer_type = Alloc; }
+  // TODO: STOP
+  else if(const Argument* p = dyn_cast<Argument>(pointer)) { 
+    if(p->getName().equals("argv")) { pointer_type = Alloc; }
+    else {
+      pointer_type = Phi;
+      const Function* F = p->getParent();
+      for(auto ui = F->user_begin(), ue = F->user_end(); ui != ue; ui++) {
+        const User* u = *ui;
+        if(const CallInst* caller = dyn_cast<CallInst>(u)) {
+          int anum = caller->getNumArgOperands();
+          int ano = p->getArgNo();
+          if(ano <= anum) {
+            OffsetPointer* base = Analysis->getOffsetPointer
+              (caller->getArgOperand(ano));
+            if(base != NULL) 
+            	new Address(this, base, Offset());
+          }
+          else {
+            /// TODO: support standard values in cases where the argument
+            /// has a standard value and does not appear in function call
+            errs() << "!: ERROR (Not enough arguments):\n";
+            errs() << *p << " " << ano << "\n";
+            errs() << *u << "\n";
+            pointer_type = Unk;
+          }
+        }
+      }
+    } 
+  }
+  else if(isa<const AllocaInst>(*pointer)) { 
+  
+  }
+  else if(const CallInst* p = dyn_cast<CallInst>(pointer)) { 
+  
+  }
+  else if(const BitCastInst* p = dyn_cast<BitCastInst>(pointer)) { 
+  
+  }
+  else if(isa<const LoadInst>(*pointer)) { 
+  
+  }
+  else if(const PHINode* p = dyn_cast<PHINode>(pointer)) { 
+  
+  }
+  else if(const GetElementPtrInst* p = dyn_cast<GetElementPtrInst>(pointer)) { 
+  
+  }
+  else if(const GEPOperator* p = dyn_cast<GEPOperator>(pointer)) { 
+  
+  }
+  else if(isa<const ConstantPointerNull>(*pointer)) { 
+  
+  }
+  else if(const ConstantExpr* p = dyn_cast<ConstantExpr>(pointer)) { 
+  
+  }
+  else if(isa<const Function>(*pointer)) { 
+  
+  }
   else { 
     pointer_type = Unk; 
   }
 }
 
+/// \brief Function that finds the pointer's path to the root of it's local
+/// tree.
+/// TODO: Optimize function to take advantage of topological ordering.
 void OffsetPointer::getPathToRoot() {
-  
+  OffsetPointer* current = this;
+  int index = 0;
+  Offset offset;
+  while(true) {
+    path_to_root[current] = std::pair<int, Offset>(index, offset);
+    if(current->addresses.size() == 1) {
+      Address* addr = *(current->addresses.begin());
+      current = addr->getBase();
+      if(path_to_root.count(current)) {
+        //This means that the local tree is actually a lonely loop
+      	// so the local tree's root will be the pointer with the highest address
+      	OffsetPointer* root = NULL;
+      	for(auto i : path_to_root){
+      		if(root < i.first) root = i.first;
+      	}
+      	local_root = root;
+      	break;
+      }
+    }
+  }
 }
 
 
